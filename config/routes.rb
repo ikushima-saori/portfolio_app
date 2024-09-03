37a5cd1c6fe0b0516bd4297e20e6918b93bef33b
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-03 06:43:45 +0000

    [fix]ルーティング修正&パスワード機能削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 end
