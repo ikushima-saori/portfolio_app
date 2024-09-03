62ccaac0cffa8ba8d3a77b523c8800db0242fce3
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-03 08:06:01 +0000

    [Add]public系コントローラ追加

---
+++ app/controllers/public/ideas_controller.rb
@@ -0,0 +1,25 @@
+class Public::IdeasController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def tags
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'rooms/create'
+    get 'rooms/show'
+  end
+  namespace :public do
+    get 'messages/create'
+  end
+  namespace :public do
+    get 'searches/search'
+  end
+  namespace :public do
+    get 'relationships/create'
+    get 'relationships/destroy'
+    get 'relationships/followers'
+    get 'relationships/followeds'
+  end
+  namespace :public do
+    get 'favorites/index'
+    get 'favorites/create'
+    get 'favorites/destroy'
+  end
+  namespace :public do
+    get 'ideas/index'
+    get 'ideas/new'
+    get 'ideas/create'
+    get 'ideas/edit'
+    get 'ideas/update'
+    get 'ideas/destroy'
+    get 'ideas/tags'
+    get 'ideas/search'
+  end
+  namespace :public do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/out'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
