70a5896f1913f5b61b22b36de093f174d0364ddc
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-04 08:47:25 +0000

    [Add]投稿編集機能

--- app/controllers/public/ideas_controller.rb
+++ app/controllers/public/ideas_controller.rb
@@ -10,18 +10,27 @@ class Public::IdeasController < ApplicationController
     @idea = Idea.new(idea_params)
     @idea.customer_id = current_customer.id
     if @idea.save
-      flash[:notice] = "ネタをメモしました！"
+      flash[:notice] = "アイデアをメモしました！"
       redirect_to edit_idea_path(@idea.id)
     else
-      flash[:notice] = "アイデアが入力されていません"
+      flash[:notice] = "アイデアを入力してください"
       render :new
     end
   end
 
   def edit
+    @idea = Idea.find(params[:id])
   end
 
   def update
+    @idea = Idea.find(params[:id])
+    if @idea.update(idea_params)
+      flash[:notice] = "変更が保存されました！"
+      redirect_to edit_idea_path(@idea.id)
+    else
+      flash[:notice] = "アイデアを入力してください"
+      render :edit
+    end
   end
 
   def destroy
