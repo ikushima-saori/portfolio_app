fd7b17ffe3fc4bebc1190481dd6047fa687a9d85
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-04 14:58:04 +0000

    [Add]ユーザー詳細&編集機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-    customers_my_page_path
+    my_page_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
