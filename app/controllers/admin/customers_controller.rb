1055bf65badbedcd62e92e735be1ef673731dbc4
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-10 08:07:50 +0000

    [Add]管理側ユーザー一覧&詳細

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,20 @@
 class Admin::CustomersController < ApplicationController
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_path
+    else
+      render :edit
+    end
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :profile_image, :preference, :weak, :email, :password, :is_active)
+    end
+
 end
