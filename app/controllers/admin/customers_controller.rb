f3f58d5024aecfe7b251038b5493e9973f9d28aa
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-18 06:50:49 +0000

    [fix]admin機能コードチェック

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,18 +1,18 @@
 class Admin::CustomersController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!  #adminログインしているか確認
   def edit
     @customer = Customer.find(params[:id])
   end
 
   def update
     @customer = Customer.find(params[:id])
-    @customer.update!(customer_params)  #!=失敗したら、falseではなくエラーメッセージを返す
+    @customer.update!(customer_params)  #!=失敗したら、falseを返すのではなく処理を中断してエラーを返すだけでいいのでif分岐ではなく[!]で対応
     redirect_to admin_path
   end
 
   private
     def customer_params
-      params.require(:customer).permit(:name, :profile_image, :preference, :weak, :email, :password, :is_active)
+      params.require(:customer).permit(:name, :profile_image, :preference, :weak, :is_active)
     end
 
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomesController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!  #adminログインしているか確認
   def top
-    @customers = Customer.all.page(params[:page]).per(4)
+    @customers = Customer.all.page(params[:page]).per(4)  #ユーザー一覧をページネーションする
   end
 end
