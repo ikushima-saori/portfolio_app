b51effda07984e57e991e9e08bbcf8ebbe4029a5
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-09-28 03:29:01 +0000

    [fix]レイアウト&インデント調整

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,15 +8,15 @@ class Public::SearchesController < ApplicationController
     @method = params[:method]  #検索方法
 
     if @model  == "customer"
-    @records = Customer.where.not(email: Customer::GUEST_USER_EMAIL)  #Customerの中にゲストユーザーは含まない
-                       .where(is_active: true)  # is_activeがtrueのユーザーのみを取得
-                       .search_for(@word, @method) # Customerモデル内を検索ワードと検索方法で検索して該当するユーザーを取得
-                       .order(:id)  #idで並べ替え
-                       .page(params[:page]).per(4)
-    @total_count = Customer.where.not(email: Customer::GUEST_USER_EMAIL)
-                           .where(is_active: true)
-                           .search_for(@word, @method)
-                           .count
+      @records = Customer.where.not(email: Customer::GUEST_USER_EMAIL)  #Customerの中にゲストユーザーは含まない
+                         .where(is_active: true)  # is_activeがtrueのユーザーのみを取得
+                         .search_for(@word, @method) # Customerモデル内を検索ワードと検索方法で検索して該当するユーザーを取得
+                         .order(:id)  #idで並べ替え
+                         .page(params[:page]).per(4)
+      @total_count = Customer.where.not(email: Customer::GUEST_USER_EMAIL)
+                             .where(is_active: true)
+                             .search_for(@word, @method)
+                             .count
     else
       @records = Idea.where(is_active: true)  # is_activeがtrueのアイデアのみを取得
                      .search_for(@word, @method) # Customerモデル内を検索ワードと検索方法で検索して該当するユーザーを取得
